f96887c9f62954110bb30c1fe843a59fccd5c5cd
AliceAqeelah <92762410+AliceAqeelah@users.noreply.github.com>
2022-02-25 23:27:59 +0200

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['submit'])) {
+    $files = $_FILES['file'];
+
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);//we get an array
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg' , 'png ' , 'pdf');
+
+    if (in_array($fileActualExt , $allowed)) {
+        if($fileError=== 0){
+            if($fileSize < 1000000){
+                $fileNameNew = uniqid('', true).".".$fileActualExt;//generates unique number at the time of upload
+                $fileDestination='uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: index.php?uploadsuccess");
+            }
+            else{
+                echo"Your file is too big!";
+            }
+        }
+        else{
+            echo"There was an error uploading this file!";
+        }
+
+    }
+    else{
+        echo"You cannot upload files of this type!";
+    }
+}
